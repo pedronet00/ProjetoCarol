9d5ad192e9f15e048c5f64327e8319b220aea231
Pedro Stabile Neto <stabilepedro010403@gmail.com>
2026-03-26 23:24:42 -0300

    feat: alterando program.cs

--- ProjetoCarol/Program.cs
+++ ProjetoCarol/Program.cs
@@ -115,5 +115,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
 
